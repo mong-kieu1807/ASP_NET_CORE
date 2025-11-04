450c19d87d4b97234d18bf652a2148e5ef1e8693
mong-kieu1807 <0306231397@caothang.edu.vn>
2025-11-05 03:06:20 +0700

    Bai tap tuan 1

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+namespace TongHop.Controllers
+{
+    public class TestController : Controller
+    {
+        // Action Method mặc định khi truy cập /Test
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Chào mừng bạn đến với Cửa hàng Thể thao!Đây là trang Test!";
+            return View(); // Trả về View có tên Index
+        }
+        // Một Action Method khác: /Test/HelloWorld
+        public IActionResult HelloWorld()
+        {
+            return Content("Xin chào từ Action HelloWorld của TestController!");
+        }
+        // Action nhận tham số: /Test/Welcome?name=DavidTeo
+        public IActionResult Welcome(string name = "Kiều")
+        {
+            return Content($"Chào mừng {name} đến với trang Test!");
+        }
+    }
+}
