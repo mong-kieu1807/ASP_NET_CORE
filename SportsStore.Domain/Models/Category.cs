ca1281bc7bbe4f495164d180c3733571890e79d3
mong-kieu1807 <0306231397@caothang.edu.vn>
2025-11-07 16:29:40 +0700

    Bài tập tuần 5

---
+++ SportsStore.Domain/Models/Category.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic; // Để dùng List
+namespace SportsStore.Domain.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        [Key]
+        [Display(Name = "Mã danh mục")]
+        public int CategoryID { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập tên danh mục.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Tên danh mục phải từ 3 đến 50 ký tự.")]
+        [Display(Name = "Tên danh mục")]
+        public string Name { get; set; } = string.Empty;
+        // Navigation property (nếu có EF Core)
+        // public virtual ICollection<Product>? Products { get; set; }
+    }
+}
